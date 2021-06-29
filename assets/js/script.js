2b52b847b40562800ef11569a028edeb99cc44da
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-06-28 21:42:08 -0500

    blocked the default browser handling for form submissions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
-var buttonEl = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    //block the browser from refreshing each time the createTaskHandler function is called. 
+    event.preventDefault();
+
     //creates new task item
 var listItemE1 = document.createElement("li");
     //styles the task item
@@ -10,7 +13,7 @@ listItemE1.className = "task-item";
 listItemE1.textContent = "This is a new task.";
      //Append this element to the task list.
 tasksToDoE1.appendChild(listItemE1);
-}
+};
 
-buttonEl.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
 
